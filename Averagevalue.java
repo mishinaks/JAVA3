36f33feee4ed859d2f9a143206ff12cd3138e0b4
christina <MISHINA.7.5@yandex.ru>
2021-02-24 14:40:26 +0300

    Задача №4_Операторы

--- Averagevalue.java
+++ Averagevalue.java
@@ -3,14 +3,14 @@ public class Averagevalue{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Input num: ");
-        int x = in.nextInt();
+        double x = in.nextDouble();
         System.out.println("Input num: ");
-        int y = in.nextInt();
+        double y = in.nextDouble();
         System.out.println("Input num: ");
-        int z = in.nextInt();
+        double z = in.nextDouble();
         double average = (x + y + z) / 3.0;
         System.out.printf( "%.2f %n" , average);
-        double a = average / 2 - average%2 / 2;
+        double a = Math.floor(average / 2);
         if (a > 3) {
             System.out.printf("Программа выполнена корректно.");
         }
